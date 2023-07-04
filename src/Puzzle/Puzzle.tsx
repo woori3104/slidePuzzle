6e162b9d3165b52777a20eb62b74e8fa09385b8a
woori <woori.kim.se@gmail.com>
2023-07-05 00:04:28 +0900

    셔플함수 분리

--- src/Puzzle/Puzzle.tsx
+++ src/Puzzle/Puzzle.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { resizeImage } from "./util";
+import { resizeImage, shuffleImage } from "./util";
 
 const PuzzleContainer = styled.div`
   display: flex;
@@ -50,7 +50,7 @@ const Puzzle: React.FC = () => {
   const [userImage, setUserImage] = useState(null);
   const [isMove, setIsMove] = useState(false);
   const [originalPuzzleState, setOriginalPuzzleState] = useState<
-    number[][] | null
+    (number|null)[][]
   >([
     [1, 2, 3],
     [4, 5, 6],
@@ -80,62 +80,10 @@ const Puzzle: React.FC = () => {
   const shufflePuzzle = () => {
     setIsMove(true);
     const flattenedPuzzle = initialState.flat();
-    const piece = flattenedPuzzle.map((p, index) => ({
-      index: index,
-      image: p,
-    }));
-    let inversions = 0;
-
-    let emptyIndex = piece.findIndex(
-      (p) => p?.image === undefined || p === undefined
-    );
-    if (emptyIndex === -1 && moveCount === 0) {
-    }
-    {
-      [piece[emptyIndex], piece[flattenedPuzzle.length - 1]] = [
-        piece[flattenedPuzzle.length - 1],
-        piece[emptyIndex],
-      ];
-    }
-
-    while (true) {
-      // Fisher-Yates 알고리즘을 사용한 무작위 셔플
-      for (let i = piece.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [piece[i], piece[j]] = [piece[j], piece[i]];
-      }
-      // 인버전 개수 다시 계산
-      inversions = 0;
-      for (let i = 0; i < piece.length; i++) {
-        for (let j = i + 1; j < piece.length; j++) {
-          if (
-            piece[i]?.index &&
-            piece[j]?.index &&
-            piece[i]?.index > piece[j]?.index
-          ) {
-            inversions++;
-          }
-        }
-      }
-      // 인버전 개수가 홀수인 경우에만 종료
-      if (inversions % 2 === 1) {
-        break;
-      }
-    }
-
-    const newArr = piece.map((p) => p?.image);
-    // 셔플된 퍼즐 배열 반환
-    const shuffledPuzzle = [
-      newArr.slice(0, 3),
-      newArr.slice(3, 6),
-      newArr.slice(6),
-    ];
-
+    const shuffledPuzzle = shuffleImage(flattenedPuzzle)
     setPuzzleState(shuffledPuzzle);
     setOriginalPuzzleState(shuffledPuzzle);
-    
     setMoveCount(0);
-
     setIsCompleted(false);
   };
   const checkCompletion = (currentState: number[][]) => {
--- src/Puzzle/util.ts
+++ src/Puzzle/util.ts
@@ -25,3 +25,57 @@ export const resizeImage = (image: string | ArrayBuffer | null, maxWidth: number
     img.src = image as string;
   });
 };
+
+export const shuffleImage = (flattenedPuzzle:(number | null)[]) => {
+  const piece = flattenedPuzzle.map((p, index) => ({
+    index: index,
+    image: p,
+  }));
+  let inversions = 0;
+
+  let emptyIndex = piece.findIndex(
+    (p) => p?.image === undefined || p === undefined
+  );
+  if (emptyIndex === -1) {
+  }
+  {
+    [piece[emptyIndex], piece[flattenedPuzzle.length - 1]] = [
+      piece[flattenedPuzzle.length - 1],
+      piece[emptyIndex],
+    ];
+  }
+
+  while (true) {
+    // Fisher-Yates 알고리즘을 사용한 무작위 셔플
+    for (let i = piece.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [piece[i], piece[j]] = [piece[j], piece[i]];
+    }
+    // 인버전 개수 다시 계산
+    inversions = 0;
+    for (let i = 0; i < piece.length; i++) {
+      for (let j = i + 1; j < piece.length; j++) {
+        if (
+          piece[i]?.index &&
+          piece[j]?.index &&
+          piece[i]?.index > piece[j]?.index
+        ) {
+          inversions++;
+        }
+      }
+    }
+    // 인버전 개수가 홀수인 경우에만 종료
+    if (inversions % 2 === 1) {
+      break;
+    }
+  }
+
+  const newArr = piece.map((p) => p?.image);
+  // 셔플된 퍼즐 배열 반환
+  const shuffledPuzzle = [
+    newArr.slice(0, 3),
+    newArr.slice(3, 6),
+    newArr.slice(6),
+  ];
+  return shuffledPuzzle
+}
